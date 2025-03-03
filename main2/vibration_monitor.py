b22962886c3e5e0731995265ded5af48d2a22997
Ignacio Medina Boyd <inmedina.cl@gmail.com>
2025-03-03 18:26:21 -0300

    Update vibration_monitor.py

--- main2/vibration_monitor.py
+++ main2/vibration_monitor.py
@@ -103,6 +103,7 @@ class VibrationMonitor:
 
             # Send to PLC
             try:
+                print('Trying to write rms')
                 self.plc.client.Write(self.plc.config.get("TAG_X", 0), [rms_x, rms_y, rms_z])
             except Exception as e:
                 print(f'❌ Failed to send RMS values to PLC: {e}')
