e80a827d0fb2b2af4ac1f25d76696c479d599d84
Ignacio Medina Boyd <inmedina.cl@gmail.com>
2025-03-21 12:49:58 -0300

    Update config.py

--- main/config.py
+++ main/config.py
@@ -1,8 +1,8 @@
 CONFIG = {
     "HOST": "192.168.168.32",
     "PORT": 65410,
-    "SAMPLING_RATE": 2000,  # Hz
-    "WINDOW_SIZE": 100,  # Samples for RMS calculation
+    "SAMPLING_RATE": 4000,  # Hz
+    "WINDOW_SIZE": 200,  # Samples for RMS calculation
     "SAVE_INTERVAL": 10000,  # Samples per chunk
     "PLC_UPDATE_INTERVAL": 0.0001,  # Seconds between RMS updates
     "THRESHOLD": 100.0,  # Acceleration threshold for alerts
